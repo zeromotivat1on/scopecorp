01c041fe47729ccc484bee8f63ded77848aa62ab
Alexander <lyannoy.alexander@gmail.com>
2026-01-11 22:43:21 +0100

    fix dev stat text draw, render.cpp cleanup, text file handler fixes, dynamic change of log level and appropriate console message draw based on its log level (fix issue with scroll position due to this new feature)

--- src/text_file.h
+++ src/text_file.h
@@ -5,51 +5,54 @@
 // Convenient handler to work with text files, useful for custom formats for example.
 struct Text_File_Handler {
     Allocator allocator = context.allocator;
-    
-    String path;
-    File   file = FILE_NONE;
-
-    String contents;
-    s64    pos = 0;
+    String    path;
+    File      file = FILE_NONE;
+    String    contents;
+    s64       pos = 0;
 };
 
-inline void reset(Text_File_Handler *text) {
-    if (text->file) {
-        close_file(text->file);
-        release(text->contents.data, text->allocator);
+inline void reset(Text_File_Handler *handler) {
+    if (handler->file) {
+        close_file(handler->file);
+        release(handler->contents.data, handler->allocator);
     }
     
-    text->path     = {};
-    text->contents = {};
-    text->pos      = 0;
+    handler->path     = {};
+    handler->contents = {};
+    handler->pos      = 0;
 }
 
-inline bool read_entire_file(Text_File_Handler *text, String path) {
-    text->file = open_file(path, FILE_READ_BIT);
-    if (!text->file) return false;
+inline bool read_entire_file(Text_File_Handler *handler, String path) {
+    handler->file = open_file(path, FILE_READ_BIT);
+    if (!handler->file) return false;
 
-    text->contents = read_text_file(path, text->allocator);
-    if (!text->contents) return false;
+    const auto size = get_file_size(handler->file);
+    handler->contents.data = (u8 *)alloc(size, handler->allocator);
+    handler->contents.size = read_file(handler->file, size, handler->contents.data);
+
+    if (size != handler->contents.size) {
+        log(LOG_WARNING, "Read different amount of bytes %llu/%llu from %S", handler->contents.size, size, path);
+    }
     
-    text->path = path;
-    text->pos  = 0;
+    handler->path = path;
+    handler->pos  = 0;
     
     return true;
 }
 
-inline void init_from_memory(Text_File_Handler *text, String contents) {
-    text->contents = contents;
-    text->pos      = 0;
+inline void init_from_memory(Text_File_Handler *handler, String contents) {
+    handler->contents = contents;
+    handler->pos      = 0;
 }
 
-inline String read_line(Text_File_Handler *text) {
-    auto s = slice(text->contents, text->pos);
+inline String read_line(Text_File_Handler *handler) {
+    auto s = slice(handler->contents, handler->pos);
     if (!s) return {};
     
     auto line = slice(s, S("\n"), S_LEFT_SLICE_BIT);
     if (!line) line = s; // get last part of string if did not find slice with new line
     
-    text->pos += line.size;
+    handler->pos += line.size;
     
     return line;
 }
